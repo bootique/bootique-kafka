1b06ac515119d1f714198fd6041d78f207e55913
Andrus Adamchik <andrus@objectstyle.com>
2016-08-04 14:56:42 +0300

    Upgrade to Bootique 0.19 #6

--- RELEASE-NOTES.md
+++ RELEASE-NOTES.md
@@ -2,3 +2,4 @@
 
 * #3 Integrate 0.10 client
 * #5 Integrate 0.10 consumer
+* #6 Upgrade to Bootique 0.19
--- bootique-kafka-client/src/main/java/io/bootique/kafka/client/KafkaClientModule.java
+++ bootique-kafka-client/src/main/java/io/bootique/kafka/client/KafkaClientModule.java
@@ -1,14 +1,26 @@
 package io.bootique.kafka.client;
 
+import com.google.inject.Binder;
 import com.google.inject.Provides;
 import com.google.inject.Singleton;
+import com.nhl.bootique.BQCoreModule;
 import com.nhl.bootique.ConfigModule;
 import com.nhl.bootique.config.ConfigurationFactory;
+
+import java.util.logging.Level;
 
 /**
  * @since 0.2
  */
 public class KafkaClientModule extends ConfigModule {
+
+    @Override
+    public void configure(Binder binder) {
+        // turn off chatty logs by default
+        BQCoreModule.contributeLogLevels(binder)
+                .addBinding("org.apache.kafka.clients.consumer.ConsumerConfig")
+                .toInstance(Level.WARNING);
+    }
 
     @Singleton
     @Provides
