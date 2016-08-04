135a3bee6063487bd778afa35e2cfa1d8722e9b6
Andrus Adamchik <andrus@objectstyle.com>
2016-08-04 14:46:06 +0300

    javadocs

--- bootique-kafka-client/src/main/java/io/bootique/kafka/client/KafkaClientFactory.java
+++ bootique-kafka-client/src/main/java/io/bootique/kafka/client/KafkaClientFactory.java
@@ -11,7 +11,29 @@ import org.apache.kafka.clients.consumer.Consumer;
  */
 public interface KafkaClientFactory {
 
+    /**
+     * Creates and returns a {@link Consumer} by merging default Bootique configuration with settings provided in a
+     * {@link ConsumerConfig} argument. Uses default Kafka cluster connection information (default == single configuration
+     * found in Bootique; if none or more than one exists, an exception is thrown). Returned Consumer needs to be
+     * closed by the calling code when it is no longer in use.
+     *
+     * @param config Configuration of consumer specific to the given method call.
+     * @param <K>    Consumed message key type.
+     * @param <V>    Consumed message value type.
+     * @return a new instance of Consumer.
+     */
     <K, V> Consumer<K, V> createConsumer(ConsumerConfig<K, V> config);
 
-    <K, V> Consumer<K, V> createConsumer(String servers, ConsumerConfig<K, V> config);
+    /**
+     * Creates and returns a {@link Consumer} by merging default Bootique configuration with settings provided in a
+     * {@link ConsumerConfig} argument. Returned Consumer needs to be closed by the calling code when it is no longer
+     * in use.
+     *
+     * @param clusterName symbolic configuration name for the Kafka cluser coming from configuration.
+     * @param config      Configuration of consumer specific to the given method call.
+     * @param <K>         Consumed message key type.
+     * @param <V>         Consumed message value type.
+     * @return a new instance of Consumer.
+     */
+    <K, V> Consumer<K, V> createConsumer(String clusterName, ConsumerConfig<K, V> config);
 }
