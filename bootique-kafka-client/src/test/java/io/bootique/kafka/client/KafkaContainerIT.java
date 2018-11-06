4ffa217da165991afb352e68a4761990f07c7ace
kkomyak <const1993@gmail.com>
2018-11-06 14:47:26 +0300

    Change using Consumer to bootique KafkaConsumerRunner.

--- bootique-kafka-client/src/test/java/io/bootique/kafka/client/KafkaContainerIT.java
+++ bootique-kafka-client/src/test/java/io/bootique/kafka/client/KafkaContainerIT.java
@@ -2,30 +2,27 @@ package io.bootique.kafka.client;
 
 import io.bootique.BQRuntime;
 import io.bootique.kafka.BootstrapServers;
+import io.bootique.kafka.client.consumer.KafkaConsumerRunner;
 import io.bootique.kafka.client.consumer.KafkaConsumersManager;
 import io.bootique.test.junit.BQTestFactory;
-import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.rnorth.ducttape.unreliables.Unreliables;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.KafkaContainer;
 
-import java.time.Duration;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Stream;
 
-import static java.time.temporal.ChronoUnit.MILLIS;
 import static org.apache.kafka.clients.producer.ProducerConfig.*;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -39,13 +36,18 @@ public class KafkaContainerIT {
     @Rule
     public BQTestFactory testFactory = new BQTestFactory();
 
+    private BQRuntime runtime;
 
-    @Test
-    public void testExternalZookeeperWithExternalNetwork() throws Exception {
-        BQRuntime runtime  = testFactory
+    @Before
+    public void before() {
+        runtime  = testFactory
                 .app("--config=classpath:config.yml")
                 .module(KafkaClientModule.class)
                 .createRuntime();
+    }
+
+    @Test
+    public void testExternalZookeeperWithExternalNetwork() throws Exception {
 
         try (
                 KafkaContainer kafka = new KafkaContainer()
@@ -59,11 +61,11 @@ public class KafkaContainerIT {
             Stream.of(kafka, zookeeper).parallel().forEach(GenericContainer::start);
 
             String bootstrapServers = kafka.getBootstrapServers();
-            testKafkaFunctionality(bootstrapServers, runtime);
+            testKafkaFunctionality(bootstrapServers);
         }
     }
 
-    private void testKafkaFunctionality(String bootstrapServers, BQRuntime runtime) throws Exception {
+    private void testKafkaFunctionality(String bootstrapServers) throws Exception {
 
         KafkaClientFactoryFactory instance = runtime.getInstance(KafkaClientFactoryFactory.class);
         KafkaConsumersManager kafkaConsumersManager = runtime.getInstance(KafkaConsumersManager.class);
@@ -76,34 +78,27 @@ public class KafkaContainerIT {
                 .producer(new StringSerializer(), new StringSerializer())
                 .property(BOOTSTRAP_SERVERS_CONFIG, bootstrapServers).create();
 
-        Consumer<String, String> consumer = instance
+        KafkaConsumerRunner<String, String> consumerRunner = instance
                 .createConsumerFactory(kafkaConsumersManager)
                 .consumer(new StringDeserializer(), new StringDeserializer())
                 .property(BOOTSTRAP_SERVERS_CONFIG, bootstrapServers)
                 .topics(topicName)
-                .create().getConsumer();
+                .create();
 
-        consumer.subscribe(Arrays.asList(topicName));
+        kafkaConsumersManager.register(consumerRunner.getConsumer());
 
         producer.send(new ProducerRecord<>(topicName, "bootique", "kafka")).get();
 
+        Iterator<ConsumerRecord<String, String>> iterator = consumerRunner.iterator();
         Unreliables.retryUntilTrue(10, TimeUnit.SECONDS, () -> {
-            ConsumerRecords<String, String> records = consumer.poll(Duration.of(300, MILLIS));
 
-            if (records.isEmpty()) {
-                return false;
-            }
-
-            assertEquals(records.count(), 1);
-            Iterator<ConsumerRecord<String, String>> iterator = records.records(topicName).iterator();
             assertTrue(iterator.hasNext());
             ConsumerRecord<String, String> record = iterator.next();
             assertEquals(record.key(), "bootique");
             assertEquals(record.value(), "kafka");
-
             return true;
         });
 
-        consumer.unsubscribe();
+        consumerRunner.close();
     }
 }
